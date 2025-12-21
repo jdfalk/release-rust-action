0c21e31279aa046286903518b2271d45b2b9fdd2
Johnathan Falk <johnathan.falk@gmail.com>
2025-12-20 21:30:59 -0500

    feat(incremental): add query cache and work products for test crate
    - Added `query-cache.bin` to store incremental compilation data.
    - Added `work-products.bin` to track work products generated during the build.
    - Created a lock file `s-he3fqj070q-12p4ixp.lock` for incremental compilation state.
    - Updated `stderr` and `stdout` files to reflect the build process and artifacts generated.
    
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

---
+++ test-project/target/debug/deps/test_crate-fc9a2965676e4c6e.d
@@ -0,0 +1,5 @@
+/Users/jdfalk/repos/github.com/jdfalk/release-rust-action/test-project/target/debug/deps/test_crate-fc9a2965676e4c6e.d: src/lib.rs
+
+/Users/jdfalk/repos/github.com/jdfalk/release-rust-action/test-project/target/debug/deps/libtest_crate-fc9a2965676e4c6e.rmeta: src/lib.rs
+
+src/lib.rs:
